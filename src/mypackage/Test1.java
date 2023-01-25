c3439c4d3e11507ff5fbeaa45532b3af99c3263a
MohanYeg <yeguru0@gmail.com>
2023-01-25 11:06:02 +0530

    first commit.....

---
+++ src/mypackage/Test1.java
@@ -0,0 +1,16 @@
+package mypackage;
+
+public class Test1 {
+
+    public static void main(String args[])
+    {
+    	String hungry = "yes";
+ 
+        if (hungry == "yes")
+            System.out.println("eat someting.......");
+        else
+            System.out.println("do work.....");
+ 
+         
+    }
+}
